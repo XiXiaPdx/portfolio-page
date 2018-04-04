5879bd54ab79934af1f7d37b17734ac60f906ee1
Xi Xia <triplextension@gmail.com>
2018-04-04 11:32:35 -0700

    added copy webpack, removed JS import image lines, and now images in src are moved to dist

--- src/main.js
+++ src/main.js
@@ -1,8 +1,18 @@
 import {PrintTest} from './extra.js';
 import '../css/styles.css';
-// import xxLogo from './img/xxlogo.svg';
-import './img/xxlogo.svg';
-import './img/appetizing900.jpg';
+
+//using CopyWebpackPlugin, all images in src are now in dist when npm starts. Below JS import statements are needed anymore. 
+
+// import './img/xxlogo.svg';
+// import './img/appetizing900.jpg';
+// import './img/appetizing700.jpg';
+// import './img/appetizing450.jpg';
+// import './img/headshot450.jpg';
+// import './img/headshot450.jpg';
+// import './img/headshot450.jpg';
+// import './img/turbo450.jpg';
+
+
 
 // var appetizingImageHolder = document.getElementById('appetizing');
 // appetizingImageHolder.src = appetizing450;
