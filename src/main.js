cc51f8baffb756b27749f343c2bb02c2113972f3
Xi Xia <triplextension@gmail.com>
2018-04-08 13:20:12 -0700

    added project cards forr 600max

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
 import {PrintTest} from './extra.js';
 import '../css/styles.css';
-// import $ from 'jquery';
+import $ from 'jquery';
 
 //using CopyWebpackPlugin, all images in src are now in dist when npm starts. Below JS import statements are needed anymore.
 
@@ -15,13 +15,13 @@ import '../css/styles.css';
 
 
 PrintTest();
-// var appetizingImageName = $('#appetizingImageName')[0];
-// var curretImageSource = $('#appetizingImage')[0].currentSrc;
-// appetizingImageName.innerHTML = curretImageSource;
-// document.getElementById('appetizingImage').onload = function(){
-//   appetizingImageName.innerHTML = $('#appetizingImage')[0].currentSrc;
-// };
-//
+var appetizingImageName = $('#appetizingImageName')[0];
+var curretImageSource = $('#appetizingImage')[0].currentSrc;
+appetizingImageName.innerHTML = curretImageSource;
+document.getElementById('appetizingImage').onload = function(){
+  appetizingImageName.innerHTML = $('#appetizingImage')[0].currentSrc;
+};
+
 // var headshotImageName = $('#headshotImageName')[0];
 // var headImageSource = $('#headshotImage')[0].currentSrc;
 // headshotImageName.innerHTML = headImageSource;
@@ -29,16 +29,16 @@ PrintTest();
 //   headshotImageName.innerHTML = $('#headshotImage')[0].currentSrc;
 // };
 //
-// var turboImageName = $('#turboImageName')[0];
-// var turboImageSource = $('#turboImage')[0].currentSrc;
-// turboImageName.innerHTML = turboImageSource;
-// document.getElementById('turboImage').onload = function(){
-//   turboImageName.innerHTML = $('#turboImage')[0].currentSrc;
-// };
-//
-// var turbosunImageName = $('#turbosunImageName')[0];
-// var turbosunImageSource = $('#turbosunImage')[0].currentSrc;
-// turbosunImageName.innerHTML = turbosunImageSource;
-// document.getElementById('turbosunImage').onload = function(){
-//   turbosunImageName.innerHTML = $('#turbosunImage')[0].currentSrc;
-// };
+var turboImageName = $('#turboImageName')[0];
+var turboImageSource = $('#turboImage')[0].currentSrc;
+turboImageName.innerHTML = turboImageSource;
+document.getElementById('turboImage').onload = function(){
+  turboImageName.innerHTML = $('#turboImage')[0].currentSrc;
+};
+
+var turbosunImageName = $('#turbosunImageName')[0];
+var turbosunImageSource = $('#turbosunImage')[0].currentSrc;
+turbosunImageName.innerHTML = turbosunImageSource;
+document.getElementById('turbosunImage').onload = function(){
+  turbosunImageName.innerHTML = $('#turbosunImage')[0].currentSrc;
+};
